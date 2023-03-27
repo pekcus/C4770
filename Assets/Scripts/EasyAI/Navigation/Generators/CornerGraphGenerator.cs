ce4a4e55ed5f373043c1d2d8090e9d74d4e24612
Nonika <veronika.reingold@gmail.com>
2023-03-27 13:38:08 -0400

    NodeGenerator stuff

--- Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -18,6 +18,31 @@ namespace EasyAI.Navigation.Generators
         public override void Generate()
         {
             // TODO - Assignment 4 - Complete corner-graph node generation.
+            //NodeArea.AddNode(6,10);
+            //NodeArea.AddNode(7,10);
+            // Loop through each node space.
+            for (int x = 0; x < NodeArea.RangeX; x++)
+            {
+                for (int z = 0; z < NodeArea.RangeZ; z++)
+                {
+                    // If a node is closed, it might be a corner.
+                    // Is it okay to assume that a node is not a corner if it's on the edge?
+                    if (!NodeArea.IsOpen(x, z) && CheckEdge(x, z))
+                    {
+                        // Find which corner it is, and check '3' spaces out.
+                        
+                    }
+                }
+            }
+        }
+
+        private bool CheckEdge(int x, int z)
+        {
+            if ( (x >= cornerNodeSteps || x <= NodeArea.RangeX - cornerNodeSteps - 1) 
+                 && (x >= cornerNodeSteps || x <= NodeArea.RangeX - cornerNodeSteps - 1) ) {
+                return true;
+            }
+            return false;
         }
     }
 }
