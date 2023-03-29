def0f0abd6e927f435f63b246b2df5ff8c66ebe7
Nonika <veronika.reingold@gmail.com>
2023-03-29 14:48:41 -0400

    [NonFunctional] Progress

--- Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -23,30 +23,54 @@ namespace EasyAI.Navigation
             List<Vector3> path = new List<Vector3>();
             List<AStarNode> nodes = new List<AStarNode>();
             AStarNode node = new AStarNode(current, goal, null);
+            nodes.Add(node);
+
+            // Visiting Nodes
+            while (nodes.Any(n => n.IsOpen)) {
+                node.Close();
+                OpenNodes(node, goal, nodes, connections);
+                node = nodes.Where(n => n.IsOpen).OrderBy(n => n.CostF).First();
+            }
+            
+            // Creating the Path
+            path.Add(node.Position);
+            while (node.Previous != null)
+            {
+                path.Add(node.Previous.Position);
+                node = node.Previous;
+            }
+            path.Add(node.Position);
+            path.Reverse();
+            // Debugging the Path
+            Debug.Log("Path from " + current.ToString() + " to " + goal.ToString());
+            for (int i = 0; i < path.Count; i++)
+            {
+                Debug.Log(path[i].x + ", " + path[i].z);
+                
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// Helper Function to open nodes
+        /// </summary>
+        /// <param name="node"> the current node </param>
+        /// <param name="goal"> the goal node </param>
+        /// <param name="nodes"> the list of nodes </param>
+        /// <param name="connections"> the list of connections </param>
+        private static void OpenNodes(AStarNode node, Vector3 goal, List<AStarNode> nodes, List<Connection> connections)
+        {
             foreach (Connection c in connections)
             {
-                if (c.A == current)
-                    nodes.Add(new AStarNode(c.B, goal, node));
-                else if (c.B == current)
-                    nodes.Add(new AStarNode(c.A, goal, node));
-            }
-
-            while (nodes.Count > 0)
-            {
-                node.Close();
-                AStarNode next = nodes.OrderBy(n => n.CostF).First();
-                next.Close();
-                node = next;
-                foreach (Connection c in connections)
+                if (!(nodes.Any(n => n.IsOpen && n.Position == c.A)) && c.A == node.Position)
                 {
-                    if ((nodes.Any(n => n.IsOpen && n.Position == c.A)) && c.A == node.Position)
-                        nodes.Add(new AStarNode(c.B, goal, node));
-                    else if (c.B == current)
-                        nodes.Add(new AStarNode(c.A, goal, node));
+                    nodes.Add(new AStarNode(c.B, goal, node));
+                }
+                if (!(nodes.Any(n => n.IsOpen && n.Position == c.B)) && c.B == node.Position)
+                {
+                    nodes.Add(new AStarNode(c.A, goal, node));
                 }
             }
-
-            return new();
         }
     }
 }
