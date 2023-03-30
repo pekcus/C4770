5f74fcc262d66b1a67d3a47256fca9d0ba10af55
Nonika Reingold <veronika.reingold@gmail.com>
2023-03-30 14:50:17 -0400

    Touch-Ups

--- Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using EasyAI.Navigation.Nodes;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace EasyAI.Navigation
@@ -45,13 +46,7 @@ namespace EasyAI.Navigation
                 node = node.Previous;
             }
             path.Reverse();
-            // Debugging the Path
-            Debug.Log("Path from " + current.ToString() + " to " + goal.ToString());
-            for (int i = 0; i < path.Count; i++)
-            {
-                Debug.Log(path[i].x + ", " + path[i].z);
-                
-            }
+            //DebugPrint(path, current, goal);
             return path;
         }
 
@@ -66,35 +61,52 @@ namespace EasyAI.Navigation
         {
             foreach (Connection c in connections)
             {
+                // Efficiency: we can skip irrelevant connections right away
                 // Skip connections that don't have the node
                 if (c.A != node.Position && c.B != node.Position)
                     continue;
                 
-                // Make a new node for comparison
+                // SR: Make a new node for comparison
                 Vector3 p = c.A == node.Position ? c.B : c.A;
                 AStarNode newNode = new(p, goal, node);
 					
-                // See if there is already an existing node at this position
+                // With valid connection and a "new" node, we can then insert the new node
+                // into the nodes list, if it does not already exist.
+                // SR: See if there is already an existing node at this position
                 AStarNode existing = nodes.FirstOrDefault(n => n.Position == p);
-                // If there is no node, this is easy and just add the new node to the list
+                // SR: If there is no node, this is easy and just add the new node to the list
                 if (existing == null)
                 {
                     nodes.Add(newNode);
                     continue;
                 }
                 
-                // Otherwise, there is an existing node
-                // If the existing node has a better (or equal) score, then just discard this new node
+                // If the node already exists, we need to compare which cost is better.
+                // The node with the best cost stays, the node with the worse cost is discarded.
+                
+                // SR: Otherwise, there is an existing node
+                // SR: If the existing node has a better (or equal) score, then just discard this new node
                 if (existing.CostF <= newNode.CostF)
                 {
                     continue;
                 }
 					
-                // Otherwise, the new node is better than the existing one
-                // update the existing node to use the new cost and
+                // SR: Otherwise, the new node is better than the existing one
+                // SR: update the existing node to use the new cost and
                 existing.UpdatePrevious(node);
                 // re-open the updated node
                 existing.Open();
+            }
+        }
+
+        private static void DebugPrint(List<Vector3> path, Vector3 current, Vector3 goal)
+        {
+            // Debugging the Path
+            Debug.Log("Path from " + current.ToString() + " to " + goal.ToString());
+            for (int i = 0; i < path.Count; i++)
+            {
+                Debug.Log(path[i].x + ", " + path[i].z);
+                
             }
         }
     }
